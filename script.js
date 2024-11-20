ffdd5c53d6abc3392df9780a06fc7a81e7d6e802
fedy14 <jeanpierrefedy@gmail.com>
2024-11-20 14:34:46 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const  menu = document.getElementById('fedy');
+const  list = document.getElementById('list');
+
+menu.addEventListener("click", ()=>{
+   
+
+    list.classList.toggle('navlist-active');
+    
+})
+
